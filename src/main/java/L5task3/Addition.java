f8bfd619b855039f1f45abc6b4427ea63ccf79b0
TeresaTao127 <taoleiluck2020@gmail.com>
2023-08-15 20:32:05 +0100

    L5task3

---
+++ src/main/java/L5task3/Addition.java
@@ -0,0 +1,9 @@
+package L5task3;
+
+public class Addition implements MathOperation {
+
+    @Override
+    public double calculate(double number1, double number2) {
+        return number1 + number2;
+    }
+}
---
+++ src/main/java/L5task3/MathOperation.java
@@ -0,0 +1,5 @@
+package L5task3;
+
+public interface MathOperation {
+    double calculate(double number1, double number2);
+}
