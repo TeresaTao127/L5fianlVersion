ddfa17a87b0cddaa1d17525b6d545aaca1394c2f
TeresaTao127 <taoleiluck2020@gmail.com>
2023-08-30 10:34:25 +0100

    L5task2 test and function update

--- src/main/java/L5task2/FizzBuzzGame.java
+++ src/main/java/L5task2/FizzBuzzGame.java
@@ -3,16 +3,28 @@ package L5task2;
 public class FizzBuzzGame {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+            if (isFizzBuzz(i)) {
                 System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
+            } else if (isFizz(i)) {
                 System.out.println("Fizz");
-            } else if (i % 5 == 0) {
+            } else if (isBuzz(i)) {
                 System.out.println("Buzz");
             } else {
                 System.out.println(i);
             }
         }
+    }
+
+    public static boolean isFizzBuzz(int i) {
+        return (i % 3 == 0 && i % 5 == 0);
+    }
+
+    public static boolean isFizz(int i) {
+        return (i % 3 == 0);
+    }
+
+    public static boolean isBuzz(int i) {
+        return (i % 5 == 0);
     }
 }
 
